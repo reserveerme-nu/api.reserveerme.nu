c1959e99fbd5a09a8e2ffc54a9cd1abec4838756
Kevin Heijboer <kevin.heijboer@gmail.com>
2020-03-11 15:52:55 +0100

    idk

--- api.reserveerme.nu/Websockets/MessageHub.cs
+++ api.reserveerme.nu/Websockets/MessageHub.cs
@@ -5,9 +5,9 @@ namespace api.reserveerme.nu.Websockets
 {
     public class MessageHub : Hub
     {
-        public async Task Send(string message)
+        public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("Send", message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
 }
