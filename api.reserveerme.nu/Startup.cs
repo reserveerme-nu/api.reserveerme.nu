350f946b84a50af66332e705dbcf8a5942378815
Kevin Heijboer <kevin.heijboer@gmail.com>
2020-03-11 15:12:48 +0100

    Hub added??

--- api.reserveerme.nu/Startup.cs
+++ api.reserveerme.nu/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.reserveerme.nu.Websockets;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.SignalR;
 
 namespace api.reserveerme.nu
 {
@@ -26,6 +28,7 @@ namespace api.reserveerme.nu
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +45,12 @@ namespace api.reserveerme.nu
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<MessageHub>("/message");
+            });
+
         }
     }
 }
---
+++ api.reserveerme.nu/Websockets/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace api.reserveerme.nu.Websockets
+{
+    public class MessageHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("Send", message);
+        }
+    }
+}
