88802978dcdb56510d98f330e3e6387aca55893b
Aaron Goes <aaron.goes@hotmail.com>
2020-04-22 09:20:00 +0200

    Revert "Status"

--- Model/ViewModels/InstantReservationViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api.reserveerme.nu.ViewModels
-{
-    public class InstantReservationViewModel
-    {
-        public int RoomId { get; set; }
-        public int Duration { get; set; }
-        public string Issuer { get; set; }
-    }
-}
